f035ad1e880336715a4a9cf753823f40f4f5dc85
Nguyen Khoa <nguyenkhoa@gmail.com>
2017-03-06 15:37:52 -0800

    add more styling to front page

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -14,9 +14,13 @@ get_header(); ?>
     <div class="container">
         <section class="home-shop">
             <h2>Shop Stuff</h2>
-
+            <?php  //shop stuff on front page
+               $arg = array( 'taxonomy' => 'product_type', 'hide_empty' => true);
+               $terms = get_terms( $arg );
+           ?>
             <section class="product-feed-container">
                 <?php
+                     $arg = array( 'taxonomy' => 'product_type', 'hide_empty' => true);
                     $terms = get_terms('product_type');
 
                     foreach ($terms as $term) : ?>
@@ -27,8 +31,7 @@ get_header(); ?>
                             <p><?php echo $term->description; ?></p>
                             <p><a href="<?php echo $url ?>"><?php echo $term->name; ?> Stuff</a></p>
                         </div>
-                    <?php endforeach;
-                ?>
+                    <?php endforeach; ?>
             </section>
         </section>
 
